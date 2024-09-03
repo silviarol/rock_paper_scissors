643d261302eb12ff567aa79611c9bdfbc0cd6fcb
silviarol <171265625+silviarol@users.noreply.github.com>
2024-09-03 16:02:41 +0200

    Remove 5 round logic

--- myscripts.js
+++ myscripts.js
@@ -38,18 +38,4 @@ function playRound() {
     }
 } 
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    console.log("Computer score: " + computerScore + " | Your score: " + humanScore);
-  }
-  if (humanScore > computerScore) {
-    console.log("CONGRATS, YOU WIN!");
-  } else if (computerScore > humanScore) {
-    console.log("COMPUTER WINS! TRY AGAIN!");
-  } else {
-    console.log("YOU TIED!");
-  }
-}
-
-playGame();
+playRound()
