b7369a307486a3a22bff25b438261701d9c6b04e
silviarol <171265625+silviarol@users.noreply.github.com>
2024-09-04 14:22:45 +0200

    Add picture files and add heading

--- myscripts.js
+++ myscripts.js
@@ -26,25 +26,31 @@ function playRound(humanChoice) {
 
   if (humanChoice === "rock" && computerChoice === "scissors") {
       display.textContent = "You win! Rock beats scissors!";
-      return ++humanScore;
+      ++humanScore;
+      return humanScore;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
       display.textContent = "You win! Scissors beats paper!";
-      return ++humanScore;
+      ++humanScore;
+      return humanScore;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
       display.textContent = "You win! Paper beats rock!";
-      return ++humanScore;
+      ++humanScore;
+      return humanScore;
   } else if (computerChoice === "scissors" && humanChoice === "paper") {
       display.textContent = "You lose! Scissors beats paper!";
-      return ++computerScore;
+      ++computerScore;
+      return computerScore;
   } else if (computerChoice === "paper" && humanChoice === "rock") {
       display.textContent = "You lose! Paper beats rock!";
-      return ++computerScore;
+      ++computerScore;
+      return computerScore;
   } else if (computerChoice === "rock" && humanChoice === "scissors") {
       display.textContent = "You lose! Rock beats scissors!";
-      return ++computerScore;
+      ++computerScore;
+      return computerScore;
   } else if (computerChoice === humanChoice) {
       display.textContent = "It's a tie!";
-  }
+  } 
 
 } 
 
